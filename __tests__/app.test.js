48a19772f1f63b3d4ef8d127ac771278147477b0
Bela Bertalan <belabertalan@gmail.com>
2022-07-04 16:46:32 +0100

    Testing for individual object match, correct properties and error with wrong ID.

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -45,4 +45,55 @@ describe("NC-Games app", () => {
         });
     });
   });
+  describe("GET /api/reviews/:review_id", () => {
+    test("Returns 200, responds with a single review.", () => {
+      //Arrange
+      const review_id = 2;
+      return request(app)
+        .get(`/api/reviews/${review_id}`)
+        .expect(200)
+        .then(({ body }) => {
+          expect(body.review).toEqual({
+            review_id: review_id,
+            title: "Jenga",
+            designer: "Leslie Scott",
+            owner: "philippaclaire9",
+            review_img_url:
+              "https://www.golenbock.com/wp-content/uploads/2015/01/placeholder-user.png",
+            review_body: "Fiddly fun for all the family",
+            category: "dexterity",
+            created_at: "2021-01-18T10:01:41.251Z",
+            votes: 5,
+          });
+        });
+    });
+    test("Responds with status 200 and a review object with the correct properties.", () => {
+      const review_id = 2;
+      return request(app)
+        .get(`/api/reviews/${review_id}`)
+        .expect(200)
+        .then(({ body }) => {
+          const { review } = body;
+          expect(review).toHaveProperty("review_id");
+          expect(review).toHaveProperty("title");
+          expect(review).toHaveProperty("review_body");
+          expect(review).toHaveProperty("designer");
+          expect(review).toHaveProperty("review_img_url");
+          expect(review).toHaveProperty("votes");
+          expect(review).toHaveProperty("category");
+          expect(review).toHaveProperty("owner");
+          expect(review).toHaveProperty("created_at");
+        });
+    });
+    test("Responds with status 404 'Bad request', when passed the wrong id.", () => {
+      const review_id = 9999;
+      return request(app)
+        .get(`/api/reviews/${review_id}`)
+        .expect(404)
+        .then(({ body }) => {
+          const { msg } = body;
+          expect(msg).toBe(`ID ${review_id} does not exist.`);
+        });
+    });
+  });
 });
--- app.js
+++ app.js
@@ -1,15 +1,22 @@
-const { getCategories } = require("./controllers/category-controller.js");
+const { getCategories } = require("./controllers/category-controller");
+const { getReviewByID } = require("./controllers/review-controller");
 const express = require("express");
 const app = express();
 
 app.use(express.json());
 
 app.get("/api/categories", getCategories);
+app.get("/api/reviews/:review_id", getReviewByID);
 
 app.use("*", (req, res) => {
   res.status(404).send({ msg: "Not found." });
+  next();
 });
 
+app.use((err, req, res, next) => {
+  res.status(404).send(err);
+  next();
+});
 app.use((err, req, res, next) => {
   console.log(err);
   res.status(500).send("Server Error! DEBUGGING");
